1be3292693c44b59a9d42f7a07cb936458ecc563
crapptrapp <128239787+crapptrapp@users.noreply.github.com>
2023-03-25 16:18:28 +0000

    read quotes with pandas

--- sadGPT.py
+++ sadGPT.py
@@ -1,5 +1,4 @@
 import streamlit as st
-#st.write('Hello world!')
 st.set_page_config(page_title="sadGPT quotes based on https://huggingface.co/datasets/Crapp/sadQuotes")#,page_icon=img)
 hide_menu_style = """
         <style>
@@ -8,7 +7,22 @@ hide_menu_style = """
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
+import pandas as pd
+quotes=pd.read_csv("https://huggingface.co/datasets/Crapp/sadQuotes/raw/main/quotes.csv",header=None)
+quotes=quotes.fillna('')
+import random
+rowZ=len(quotes)
+rowN=65#check for one of the empty quotes
+rowN=random.randint(0,rowZ-1)
+quoteS=''.join(quotes.loc[[rowN]][1])
+quoteS=quoteS.strip()
+quoteS=" ".join(quoteS.split())
+quoteL=''.join(quotes.loc[[rowN]][0])#.to_string()
+collectI="What is purpose of Life?"
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-quote=st.text_input("What is purpose of Life?", "Purpose of Life is to have no Purpose.")
-st.write("Trying transformer on...")
-st.write(quote)
+st.write("Trying #sadGPT on ",quoteL)
+if quoteS: st.write("\"",quoteS,"\"")
+else:  st.write("emptiness...")
+collectO=st.text_input("Lets try #sadGPT on our own, like",collectI)
+if collectO==collectI: st.write("Purpose of Life is to have no Purpose.")
+else: st.write(collectO)
