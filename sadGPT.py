8859d04e9ce16033094d5c6662d8f17b1a8d65a8
crapptrapp <128239787+crapptrapp@users.noreply.github.com>
2023-03-25 18:30:43 +0000

    fixing the seed to current day

--- sadGPT.py
+++ sadGPT.py
@@ -1,5 +1,6 @@
 import streamlit as st
 st.set_page_config(page_title="sadGPT quotes based on https://huggingface.co/datasets/Crapp/sadQuotes")#,page_icon=img)
+#st.title("sadGPT quotes based on https://huggingface.co/datasets/Crapp/sadQuotes")
 hide_menu_style = """
         <style>
         MainMenu {visibility: hidden; }
@@ -8,17 +9,32 @@ hide_menu_style = """
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 import pandas as pd
-quotes=pd.read_csv("https://huggingface.co/datasets/Crapp/sadQuotes/raw/main/quotes.csv",header=None)
-quotes=quotes.fillna('')
+#https://docs.streamlit.io/library/get-started/create-an-app
+#@st.cache_data
+def load_data(URL):
+  quotes=pd.read_csv(URL,header=None)
+  quotes=quotes.fillna('')
+  return quotes
+quotes=load_data("https://huggingface.co/datasets/Crapp/sadQuotes/raw/main/quotes.csv")
 import random
 rowZ=len(quotes)
 rowN=65#check for one of the empty quotes
+rowN=356#longest
+rowN=1578#shortest
+import datetime
+current_time = datetime.datetime.now()
+st.text(current_time)
+seed=current_time.day
+random.seed(seed)
 rowN=random.randint(0,rowZ-1)
 quoteS=''.join(quotes.loc[[rowN]][1])
 quoteS=quoteS.strip()
 quoteS=" ".join(quoteS.split())
 quoteL=''.join(quotes.loc[[rowN]][0])#.to_string()
-collectI="What is purpose of Life?"
+#set_seed(42)
+#from transformers import pipeline, set_seed
+#generator = pipeline('text-generation', model='gpt2')
+collectI="What is the purpose of Life?"
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 st.write("Trying #sadGPT on ",quoteL)
 if quoteS: st.write("\"",quoteS,"\"")
